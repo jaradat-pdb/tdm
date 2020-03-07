c2f22ec77a71077fa09dfa496f747d97ecfb7e62
jaradat-pdb <jaradat.pdb@gmail.com>
2020-03-07 14:12:02 +0200

    Rename controllers package to controller

--- todo-manager/src/main/java/org/pdbcorp/mgmt/todo/controller/IndexController.java
+++ todo-manager/src/main/java/org/pdbcorp/mgmt/todo/controller/IndexController.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.pdbcorp.mgmt.todo.controllers;
+package org.pdbcorp.mgmt.todo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
